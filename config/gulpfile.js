611b6fe8f0d989f0bdab493c92cd25364893dac0
chunsong.ycs <chunsong.ycs@alibaba-inc.com>
2019-01-19 23:35:40 +0800

    stream ui

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -3,16 +3,10 @@ var gulp = require('gulp');
 var babel = require("gulp-babel");
 
 var CONFIG_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(CONFIG_PATH, '../src');
+var SRC_PATH = path.resolve(CONFIG_PATH, '../src');
 
-// main process 的编译
-gulp.task('babel:electron-main', function () {
-  return gulp.src([APP_PATH + '/main/main.js', APP_PATH + '/main/**.js'], { base: APP_PATH })
-    .pipe(babel())
-    .pipe(gulp.dest('dist'));
-});
 gulp.task('default', function () {
-  return gulp.src([APP_PATH + '/main/main.js', APP_PATH + '/main/**.js'], { base: APP_PATH })
+  return gulp.src([SRC_PATH + '/main/main.js', SRC_PATH + '/main/**.js'], { base: SRC_PATH })
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
